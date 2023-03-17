235dd54f6068b9cee01e6f2900ec9f3ee4bbd551
LuisaCostantini <126901462+LuisaCostantini@users.noreply.github.com>
2023-03-17 17:50:30 +0100

    Update R_secondcode.R

--- R_secondcode.R
+++ R_secondcode.R
@@ -13,3 +13,30 @@ pdf("myfirstgraph.pdf")
 plot(l2011$B4_sre, col=cl)
 dev.off()
 # Plotting several bands in a multiframe 
+
+
+
+
+
+
+
+# RGB plotting 
+plotRGB(l2011, r=3, g=2, b=1, stretch="lin")
+plotRGB(l2011, r=4, g=3, b=2, stretch="lin")
+plotRGB(l2011, r=3, g=4, b=2, stretch="lin")
+plotRGB(l2011, r=3, g=2, b=4, stretch="lin")
+
+# Multiframe with natural and false colors 
+par(mfrow=c(2,1))
+plotRGB(l2011, r=3, g=2, b=1, stretch="lin")
+plotRGB(l2011, r=4, g=3, b=2, stretch="lin")
+
+#Hinstogram stretching
+par(mfrow=c(2,1))
+plotRGB(l2011, r=3, g=2, b=1, stretch="Hist")
+plotRGB(l2011, r=4, g=3, b=2, stretch="Hist")
+
+par(mfrow=c(2,1))
+plotRGB(l2011, r=4, g=3, b=2, stretch="lin")
+plotRGB(l2011, r=4, g=3, b=2, stretch="Hist")
+
