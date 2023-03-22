7a40e3a957e1883a1c17d87610c664514df92a86
LuisaCostantini <126901462+LuisaCostantini@users.noreply.github.com>
2023-03-22 15:04:15 +0100

    Update R_secondcode.R

--- R_secondcode.R
+++ R_secondcode.R
@@ -1,5 +1,13 @@
 Library(raster)
-#Import ........
+#Import 
+l2011 <- brick("p224r63_2011.grd")
+l2011
+
+# plot
+plot(l2011)
+# https://www.r-graph-gallery.com/42-colors-names.html
+cl <- colorRampPalette(c("black", "grey", "light grey")) (100)
+plot(l2011, col=cl)
 
 #Exercise : change the colour gamut for all the image 
 #Exercise : plot the NIR band 
@@ -13,12 +21,28 @@ pdf("myfirstgraph.pdf")
 plot(l2011$B4_sre, col=cl)
 dev.off()
 # Plotting several bands in a multiframe 
+par(mfrow=c(2,1))
+plot(l2011[[1]])
+plot(l2011[[2]])
 
+par(mfrow=c(2,1))
+plot(l2011$B1_sre, col=cl)
+plot(l2011$B2_sre, col=cl)
 
-
-
-
-
+par(mfrow=c(2,2))
+#plot blue
+clb <- colorRampPalette(c("cyan", "blue2", "blue4")) (100) #100 sono le sfumature
+plot(l2011$B1_sre, col=clb)
+#plot green
+clg <- colorRampPalette(c("chartreuse4", "chartreuse2", "chartreuse")) (100) #100 sono le sfumature
+plot(l2011$B2_sre, col=clg)
+#plot red
+clr <- colorRampPalette(c("brown", "brown3", "brown4")) (100) #100 sono le sfumature
+plot(l2011$B3_sre, col=clr)
+#plot NIR
+cln <- colorRampPalette(c("azure", "azure4", "black")) (100) #100 sono le sfumature
+plot(l2011$B4_sre, col=cln)
+dev.off()
 
 # RGB plotting 
 plotRGB(l2011, r=3, g=2, b=1, stretch="lin")
@@ -39,4 +63,4 @@ plotRGB(l2011, r=4, g=3, b=2, stretch="Hist")
 par(mfrow=c(2,1))
 plotRGB(l2011, r=4, g=3, b=2, stretch="lin")
 plotRGB(l2011, r=4, g=3, b=2, stretch="Hist")
-
+dev.off()
