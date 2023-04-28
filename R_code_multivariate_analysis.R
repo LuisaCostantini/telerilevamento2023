5aa5c655b7425acce64316b27c92e9f1ac2e07fc
LuisaCostantini <126901462+LuisaCostantini@users.noreply.github.com>
2023-04-28 17:10:05 +0200

    Update R_code_multivariate_analysis.R

--- R_code_multivariate_analysis.R
+++ R_code_multivariate_analysis.R
@@ -1,36 +1,42 @@
-# R_code_multivariate_analysis.r
+# R code multivariate analysis 
+#How to reduce a multidimensional dataset 
 
 library(raster)
 library(ggplot2)
+library(viridis)
 
 #Import the working directory 
 
 setwd("C:/lab/") # Windows
 
-sen <- brick("sentinel.png")
+# Exercise: import the Similaun image (glacier) 
 
+sen <- brick("sentinel.png")
 sen
 plot(sen)
+
+# We combine the 3 bands (the first, the second and the third) with the function "stack"
 
 sen2 <- stack(sen[[1]], sen[[2]], sen[[3]])
 
 plot(sen2)
 
+#We use pairs for a comparison of various variables in our dataset
 pairs(sen2)
 
 # PCA (Principal Component Analysis)
 sample <- sampleRandom(sen2, 10000)
 pca <- prcomp(sample)
 
-# variance explained
+# variance explained: 
 summary(pca)
 
 # correlation with original bands
 pca
 
-# pc map
+# pc map: we visualize starting from the analysis of the pca
 pci <- predict(sen2, pca, index=c(1:2))
-
+plot(pci)
 plot(pci[[1]])
 
 # ggplot
@@ -39,6 +45,16 @@ pcid <- as.data.frame(pci[[1]], xy=T)
 ggplot() +
 geom_raster(pcid, mapping = aes(x=x, y=y, fill=PC1)) +
 scale_fill_viridis()
+
+#magma
+ggplot() +
+  geom_raster(pcid, mapping = aes(x=x, y=y, fill=PC1)) +
+  scale_fill_viridis(option = "magma")
+
+#Inferno 
+ggplot() +
+  geom_raster(pcid, mapping = aes(x=x, y=y, fill=PC1)) +
+  scale_fill_viridis(option = "inferno")
 
 
 # speeding up analyses
