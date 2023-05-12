afa5b6dfb27fdb657e4fa65b653246f7a29fe202
LuisaCostantini <126901462+LuisaCostantini@users.noreply.github.com>
2023-05-12 18:09:23 +0200

    Update R_Code_Copernicus.R

--- R_Code_Copernicus.R
+++ R_Code_Copernicus.R
@@ -12,3 +12,23 @@ library(viridis)
 
 #Import the working directory 
 setwd("C:/lab/")
+
+dataset: c_gls_SSM1km_202305090000_CEURO_S1CSAR_V1.2.1.nc
+
+ssoil <- raster("c_gls_SSM1km_202305090000_CEURO_S1CSAR_V1.2.1.nc")
+
+ssoil
+
+plot(ssoil)
+
+ssoil <- as.data.frame(ssoil, xy=T)
+
+head(ssoil)
+
+ ggplot() +
+   geom_raster(ssoil, mapping=aes(x=x, y=y, fill=Surface.Soil.Moisture))
+   #ggtitle: ("Surface.Soil.Moisture")
+   
+   #Cropping an image 
+   ext <- c(-23, 30, 62, 68)
+   
