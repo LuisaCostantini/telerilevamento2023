7c887e0df0fed4568c95f4fc56aaf841cab7dda9
LuisaCostantini <126901462+LuisaCostantini@users.noreply.github.com>
2023-05-18 16:32:36 +0200

    Create R_code_sdm.R

---
+++ R_code_sdm.R
@@ -0,0 +1,8 @@
+# Species Distribution Modelling
+#R code distribution modelling 
+install.packages("sdm")
+install.packages("rgdal", dependencies=T)
+
+library(sdm)
+library(raster) # predictors
+library(rgdal)
