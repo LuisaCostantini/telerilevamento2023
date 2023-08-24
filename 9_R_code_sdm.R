ab69559627f146748485c94dc0ebee8a5c4a01a1
LuisaCostantini <126901462+LuisaCostantini@users.noreply.github.com>
2023-08-24 18:00:37 +0200

    Update 9_R_code_sdm.R

--- 9_R_code_sdm.R
+++ 9_R_code_sdm.R
@@ -1,32 +1,37 @@
-# Species Distribution Modelling
+# SPECIES DISTRIBUTION MODELLING
 #R code distribution modelling 
+
+#Let’s download these packages
 install.packages("sdm")
 install.packages("rgdal", dependencies=T)
 
+# Load the required packages
 library(sdm)
 library(raster) # predictors
 library(rgdal) #species; to manage your vector type data (points)
 
+#Load a file found within the sdm package
 file <- system.file("external/species.shp", package="sdm")
 file
 species <- shapefile(file)
 
-# looking at the set
+# Looking at the set
 species
 
 # plot
 plot(species)
 plot(species, pch=19)
 
-# looking at the occurrences  if there is a species then the presence or absence of a species the values are either 0 -> absent, instead 1 (point) the species I sampled is present 
+# Looking at the occurrences : if there is a species then the presence or absence of a species the values are either 0 -> absent, 
+#instead 1 (point) the species I sampled is present 
 species$Occurrence
 
-#We take those points where the occurrence is worth 1 then the presence of the species and plot
+# Plot the presences, 1 = presence
 presences <- species[species$Occurrence == 1,]
 presences #94 attendance of the entire dataset 
 plot(presences, col='blue',pch=16)
 
-#Let’s see now the absences (where it is 0 so) and plot 
+# Plot the absences, 0 = absence
 #There are 106 absences 
 absences <- species[species$Occurrence == 0,]
 absences
@@ -36,40 +41,44 @@ plot(absences, col='red',pch=16)
 plot(species[species$Occurrence == 1,],col='blue',pch=16) #presences
 points(species[species$Occurrence == 0,],col='red',pch=16)#absences
 
-# predictors: look at the path
+# Predictors: look at the path
 path <- system.file("external", package="sdm")
 
-# list the predictors
+# List the predictors
 lst <- list.files(path=path,pattern='asc$',full.names = T) #
 lst
 
-# stack
+#Stack
 preds <- stack(lst)
 preds
 plot(preds) 
 
-#preds are environmental variables and are related to predictions 
-# plot preds 
+# Preds are environmental variables and are related to predictions 
+# Plot preds 
 cl <- colorRampPalette(c('blue','orange','red','yellow')) (100)
 plot(preds, col=cl)
 
-# plot predictors and occurrences
+# Plot predictors and occurrences
 plot(preds$elevation, col=cl)
-points(species[species$Occurrence == 1,], pch=19)
+points(species[species$Occurrence == 1,], pch=19) #this species occur in lower altitudes
 
 plot(preds$temperature, col=cl)
 points(species[species$Occurrence == 1,], pch=19)#this species loves medium/high t 
 
 plot(preds$precipitation, col=cl)
-points(species[species$Occurrence == 1,], pch=19)
+points(species[species$Occurrence == 1,], pch=19) #and where it's quite moist
 
 plot(preds$vegetation, col=cl)
-points(species[species$Occurrence == 1,], pch=19)#Species that prefers areas with highest plant cover 
+points(species[species$Occurrence == 1,], pch=19) #Species that prefers areas with highest plant cover 
+# They're called predictors because they predict patterns
 
-# model to see how to create a continuous map of species distribution in space 
+
+
+# Model to see how to create a continuous map of species distribution in space 
 #Species data on the ground are called train and predictors are environmental
 # set the data for the sdm
 
+# Setting data for sdm
 datasdm <- sdmData(train=species, predictors=preds)
 datasdm
 
@@ -77,8 +86,7 @@ datasdm
 m1 <- sdm(Occurrence ~ elevation + precipitation + temperature + vegetation, data=datasdm, methods = "glm")
 m1
 
-# make the raster output layer 
-# predict creates a map of species distribution
+# make the raster output layer , predict creates a map of species distribution
 p1 <- predict(m1, newdata=preds)
 p1
 
